d70f6676decc6d6ced930a92143b27ae6e4910bc
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-04-29 15:58:41 -0400

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+partner_a = Partner.create(name: "Rodney Peak", title: "Co-Founder")
+partner_b = Partner.create(name: "James Hyatt", title: "Co-Founder")
+partner_c = Partner.create(name: "Dustin Ball", title: "Partner Level 1")
+partner_d = Partner.create(name: "Bill Julian", title: "CFO")
+
+responsibility_a = Responsibility.create(partner_id: "0", task: "Produce Content")
+responsibility_b = Responsibility.create(partner_id: "1", task: "Create Marketing Plan")
+responsibility_c = Responsibility.create(partner_id: "2", task: "Aquire Paying Clients")
+responsibility_d = Responsibility.create(partner_id: "3", task: "Oopen Bank Account")
