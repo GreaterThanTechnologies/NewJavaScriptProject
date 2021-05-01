1ac97971dc8d7cdfb81af12519f573ca39a3ba62
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-05-01 15:52:11 -0400

    removed all template files from project

--- app/views/responsibility/index.js
+++ app/views/responsibility/index.js
@@ -1,5 +1,11 @@
-fetch('http://localhost:3000/responsibilities')
+let partnerResponsibilities = fetch('http://localhost:3000/responsibilities')
   .then(resp => resp.json())
   .then(json => console.log(json))
 
-  
+let partners = fetch('http://localhost:3000/partners')
+  .then(resp => resp.json())
+  .then(json => console.log(json))
+ 
+function partner(partners) {
+  console.log(partners)
+}
