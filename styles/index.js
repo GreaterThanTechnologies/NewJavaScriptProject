1117cb3b3ea4204b66a3dabedfd81904ffe29f1d
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-05-02 12:30:46 -0400

    adds styling and action column for the     delete and edit functions of partners

--- styles/index.js
+++ styles/index.js
@@ -1,11 +1,23 @@
-let partnerResponsibilities = fetch('http://localhost:3000/responsibilities')
+function fetchResponsibilities() {
+  return fetch('http://localhost:3000/responsibilities')
   .then(resp => resp.json())
   .then(json => console.log(json))
-
-let partners = fetch('http://localhost:3000/partners')
-  .then(resp => resp.json())
-  .then(json => console.log(json))
- 
-function partner(partners) {
-  console.log(partners)
 }
+
+function renderResponsibilities(responsibilities) {
+  let partnerRes = document.querySelector('main')
+  responsibilities.forEach(responsibility => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = responsibilities.task
+    main.appendChild(h2)
+  })
+}
+
+function fetchPartners() {
+  return fetch('http://localhost:3000/partners')
+  .then(resp => resp.json())
+  .then(json => console.log(json))
+}
+console.log("James is a Ninja")
+fetchResponsibilities()
+fetchPartners()
